1e53966f8658aaa47948b55257900d046458d105
Juanito Banca <jose_alvarado89@hotmail.com>
2021-06-16 21:05:34 -0700

    dash_html_components

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # import dash and bootstrap components
 import dash
 import dash_bootstrap_components as dbc
-
+import dash_html_components as html
 
 # set app variable with dash, set external style to bootstrap theme
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SANDSTONE],
