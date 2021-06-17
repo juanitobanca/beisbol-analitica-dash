b3d3b8d3c698e2ba75ea9d98a0dbd38a0042a06d
Juanito Banca <jose_alvarado89@hotmail.com>
2021-06-16 20:25:01 -0700

    Procfile

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ app.layout = html.Div([
 
 @app.callback(dash.dependencies.Output('display-value', 'children'),
               [dash.dependencies.Input('dropdown', 'value')])
+
 def display_value(value):
     return 'You have selected "{}"'.format(value)
 
