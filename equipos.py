f1802bc01be706404fbc9912a9a7f1d2fac93b23
Juanito Banca <jose_alvarado89@hotmail.com>
2021-06-17 23:37:15 -0700

    agg_batting_stats.csv

---
+++ equipos.py
@@ -0,0 +1,10 @@
+# Dash components, html, and dash tables
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+
+# Import Bootstrap components
+import dash_bootstrap_components as dbc
+
+# Import custom data.py
+import data
