32d6531e95ea57b5918352524b8d5e588f813a20
Juanito Banca <jose_alvarado89@hotmail.com>
2021-06-18 19:35:53 -0700

    equipos

--- equipos.py
+++ equipos.py
@@ -8,11 +8,11 @@ import dash_table
 import data as d
 
 # Main applicaiton menu
-layout = html.Div([
-    dbc.Row( children =
+layout = html.Div(
+    children =
         [
-            html.Label(['Temporada'], style={'font-weight': 'bold', "text-align": "center"}),
-            dbc.Col(dcc.Dropdown(
+            html.P("Select Clinic"),
+            dcc.Dropdown(
                 style = {'text-align': 'center', 'font-size': '12px', 'width': '250px'},
                 id='seasons-dropdown',
                 options=d.lov_seasons,
@@ -20,20 +20,13 @@ layout = html.Div([
                 clearable=False,
                 placeholder="Selecciona una Temporada",
                 ),
-                xs={'size':'auto', 'offset':0}, sm={'size':'auto', 'offset':0}, md={'size':'auto', 'offset':0},
-                lg={'size':'auto', 'offset':0}, xl={'size':'auto', 'offset':0}),
-            dbc.Col(html.H2(style={'text-align': 'center', 'font-size': '12px'}, children='Equipo '),
-                xs={'size':'auto', 'offset':0}, sm={'size':'auto', 'offset':0}, md={'size':'auto', 'offset':3},
-                lg={'size':'auto', 'offset':0}, xl={'size':'auto', 'offset':0}),
-            dbc.Col(dcc.Dropdown(
+            html.P("Select Team"),
+            dcc.Dropdown(
                 style = {'text-align': 'center', 'font-size': '12px', 'width': '250px'},
                 id='teams-dropdown',
                 options=d.lov_teams,
                 value=d.lov_teams[0]['value'],
-                clearable=False),
-                xs={'size':'auto', 'offset':0}, sm={'size':'auto', 'offset':0}, md={'size':'auto', 'offset':0},
-                lg={'size':'auto', 'offset':0}, xl={'size':'auto', 'offset':0}),
-        ],
-            justify="center",
-        ),
-],className = 'app-page')
+                clearable=False
+                ),
+        ]
+)
