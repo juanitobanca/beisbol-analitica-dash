c07a63fe088b9ab02e7e45cb097d7c30f29acfcc
Juanito Banca <jose_alvarado89@hotmail.com>
2021-06-20 15:22:54 -0700

    Update callbacks.py

--- callbacks.py
+++ callbacks.py
@@ -1,3 +1,13 @@
 from dash.dependencies import Input, Output
 import data as d
 from app import app
+
+# callbacks
+@app.callback(
+    Output('teams-dropdown', 'children'),
+    [Input('majorleagues-dropdown', 'value')])
+def filter_teams_from_majorleagues(fval):
+    scol = 'teamName'
+    fcol = 'majorLeague'
+    df = d.agg_batting_stats
+    df = d.filter_dataset( df, scol, fcol, fval )
