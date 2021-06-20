33e84693770f9b220e389aca96bdfad9ccf9a1e4
Juanito Banca <jose_alvarado89@hotmail.com>
2021-06-20 15:00:24 -0700

    Update callbacks.py

--- callbacks.py
+++ callbacks.py
@@ -11,4 +11,3 @@ def filter_teams_from_majorleagues(fval):
     fcol = 'majorLeague'
     df = d.agg_batting_stats
     df = d.filter_dataset( df, scol, fcol, fval )
-    logging.debug(f'Got {fval}')
