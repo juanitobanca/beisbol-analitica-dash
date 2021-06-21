21ab9cb9ba626dc0fe3d208db1f2a8e8fa541c57
Juanito Banca <jose_alvarado89@hotmail.com>
2021-06-20 18:07:26 -0700

    Update callbacks.py

--- callbacks.py
+++ callbacks.py
@@ -1,6 +1,7 @@
 from dash.dependencies import Input, Output
 import data as d
 from app import app
+import logging
 
 # callbacks
 @app.callback(
@@ -8,13 +9,14 @@ from app import app
     [Input('lov_majorLeague', 'value'), Input('lov_season', 'value') ]
     )
 def set_team_from_majorleague(lov_majorLeague=None, lov_season=None):
-    df = d.agg_batting_stats
     scol = 'teamName'
     col_val = { 'majorLeague' : lov_majorLeague,
                 'seasonId' : lov_season
               }
 
-    df = d.filter_df(df, col_val)
+    df = d.filter_df(d.agg_batting_stats, col_val)
     lov = d.create_list_of_values( df, scol )
+
+    logging.debug(lov)
 
     return lov
