ef2eda56d380d30e80a7049411fa0b6abaa2f79e
Juanito Banca <jose_alvarado89@hotmail.com>
2021-06-25 18:48:52 -0700

    Lov Specs

--- data.py
+++ data.py
@@ -1,17 +1,18 @@
 import pandas as pd
 
 # functions
-def create_list_of_values( df, lcol, vcol ):
+def create_list_of_values(df, lcol, vcol):
 
     lov = []
     unique_values = df.drop_duplicates([lcol, vcol])
 
-    for idx, row  in unique_values.iterrows():
-        lov.append({'label': row[lcol], 'value': row[vcol] })
+    for idx, row in unique_values.iterrows():
+        lov.append({"label": row[lcol], "value": row[vcol]})
 
     return lov
 
-def filter_df( df, fcols ):
+
+def filter_df(df, fcols):
 
     filters = []
 
@@ -22,36 +23,17 @@ def filter_df( df, fcols ):
 
     return df
 
+
 # datasets
 dataset_specs = {
-    'agg_batting_stats' : { 'path' :'data/agg_batting_stats.csv',
-                          },
-    'agg_team_performance_stats' : { 'path' :'data/agg_team_performance_stats.csv',
-                                    }
+    "agg_batting_stats": {
+        "path": "data/agg_batting_stats.csv",
+    },
+    "agg_team_performance_stats": {
+        "path": "data/agg_team_performance_stats.csv",
+    },
 }
 
 for dataset, specs in dataset_specs.items():
-    df = pd.read_csv( filepath_or_buffer = specs['path'] )
-    dataset_specs[dataset]['dataset'] = df
-
-# list of values specs
-lov_specs = {
-    'lov_team' : { 'dataset' : dataset_specs['agg_team_performance_stats']['dataset'],
-                   'id' : 'lov_team',
-                   'lcol' : 'teamName',
-                   'vcol' : 'teamId',
-    },
-    'lov_season' : { 'dataset' : dataset_specs['agg_team_performance_stats']['dataset'],
-                   'id' : 'lov_season',
-                   'lcol' : 'seasonId',
-                   'vcol' : 'seasonId',
-    },
-    'lov_majorLeague' : { 'dataset' : dataset_specs['agg_team_performance_stats']['dataset'],
-                          'id' : 'lov_majorLeague',
-                          'lcol' : 'majorLeague',
-                          'vcol' : 'majorLeagueId',
-                        }
-}
-
-for lov, specs in lov_specs.items():
-    lov_specs[lov]['options'] = create_list_of_values( df = specs['dataset'], lcol = specs['lcol'], vcol = specs['vcol'])
+    df = pd.read_csv(filepath_or_buffer=specs["path"])
+    dataset_specs[dataset]["dataset"] = df
--- equipos/layout.py
+++ equipos/layout.py
@@ -1,47 +1,88 @@
+#!/usr/bin/python
 # Dash components, html, and dash tables
+
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import dash_table
 
+from dash.dependencies import Input, Output
+
 # Custom dependencies
+
 import data as d
 
+# Layout Children
+children = []
+
+# List of Values
+
+lov_specs = {
+    "lov_team": {
+        "dataset": dataset_specs["agg_team_performance_stats"]["dataset"],
+        "id": "lov_team",
+        "lcol": "teamName",
+        "vcol": "teamId",
+        "label": "Equipo",
+        "style": {"text-align": "center", "font-size": "12px", "width": "250px"},
+        "value": None,
+        "clearable": False,
+        "placeholder": "Selecciona un Equipo",
+        "multi": True,
+        "callback_output": Output("lov_team", "options"),
+        "callback_input": [
+            Input("lov_majorLeague", "value"),
+            Input("lov_season", "value"),
+        ],
+    },
+    "lov_season": {
+        "dataset": dataset_specs["agg_team_performance_stats"]["dataset"],
+        "id": "lov_season",
+        "lcol": "seasonId",
+        "vcol": "seasonId",
+        "label": "Temporada",
+        "style": {"text-align": "center", "font-size": "12px", "width": "250px"},
+        "value": None,
+        "clearable": False,
+        "placeholder": "Selecciona una Temporada",
+        "multi": True,
+    },
+    "lov_majorLeague": {
+        "dataset": dataset_specs["agg_team_performance_stats"]["dataset"],
+        "id": "lov_majorLeague",
+        "lcol": "majorLeague",
+        "vcol": "majorLeagueId",
+        "label": "Liga",
+        "style": {"text-align": "center", "font-size": "12px", "width": "250px"},
+        "value": None,
+        "clearable": False,
+        "placeholder": "Selecciona una Liga",
+        "multi": True,
+    },
+}
+
+for (lov, specs) in lov_specs.items():
+
+    # Set options
+    lov_specs[lov]["options"] = create_list_of_values(
+        df=specs["dataset"], lcol=specs["lcol"], vcol=specs["vcol"]
+    )
+
+    # Set component
+    children.append(html.Br())
+    children.append(html.P(specs["label"]))
+    children.append(
+        dcc.Dropdown(
+            style=specs["style"],
+            id=specs["id"],
+            options=specs["options"],
+            value=specs["value"],
+            clearable=specs["clearable"],
+            placeholder=specs["placeholder"],
+            multi=specs["multi"],
+        )
+    )
+
 # Main application menu
-layout = html.Div(
-    children =
-        [
-            html.Br(),
-            html.P("Temporada"),
-            dcc.Dropdown(
-                style = {'text-align': 'center', 'font-size': '12px', 'width': '250px'},
-                id='lov_season',
-                options=d.lov_specs['lov_season']['options'],
-                value=None,
-                clearable=False,
-                placeholder="Selecciona una Temporada",
-                ),
-            html.Br(),
-            html.P("Liga"),
-            dcc.Dropdown(
-                style = {'text-align': 'center', 'font-size': '12px', 'width': '250px'},
-                id='lov_majorLeague',
-                options=d.lov_specs['lov_majorLeague']['options'],
-                value=None,
-                clearable=False,
-                placeholder="Selecciona una Liga",
-                ),
-            html.Br(),
-            html.P("Equipo"),
-            dcc.Dropdown(
-                style = {'text-align': 'center', 'font-size': '12px', 'width': '250px'},
-                id='lov_team',
-                options=d.lov_specs['lov_team']['options'],
-                value=None,
-                clearable=False,
-                placeholder="Selecciona un Equipo",
-                multi=True
-                )
-        ]
-)
+layout = html.Div(children=children)
