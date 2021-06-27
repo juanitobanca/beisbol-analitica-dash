72744b27f85bb3b91c98cfaaba64f8f5f0316c3e
Juanito Banca <jose_alvarado89@hotmail.com>
2021-06-26 22:45:26 -0700

    a

--- equipos/layout.py
+++ equipos/layout.py
@@ -1,10 +1,7 @@
 # Dash components, html, and dash tables
 
-import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
-import dash_table
 
 # Custom dependencies
 from equipos.specs import object_specs
@@ -18,21 +15,26 @@ for (lov, specs) in object_specs.items():
     """
     Abstract This
     """
+    if specs["object_type"] == "lov":
 
-    # Set component
-    children.append(html.Br())
-    children.append(html.P(specs["P"]))
-    children.append(
-        dcc.Dropdown(
-            style=specs["style"],
-            id=specs["id"],
-            options=specs["options"],
-            value=specs["value"],
-            clearable=specs["clearable"],
-            placeholder=specs["placeholder"],
-            multi=specs["multi"],
+        # Set component
+        children.append(html.Br())
+        children.append(html.P(specs["P"]))
+        children.append(
+            dcc.Dropdown(
+                style=specs["style"],
+                id=specs["id"],
+                options=specs["options"],
+                value=specs["value"],
+                clearable=specs["clearable"],
+                placeholder=specs["placeholder"],
+                multi=specs["multi"],
+            )
         )
-    )
+
+    elif specs["object_type"] == "fig":
+
+        children.append(dcc.Graph(id=specs["id"], figure=specs["fig"]))
 
 # Main application menu
 layout = html.Div(children=children)
--- equipos/specs.py
+++ equipos/specs.py
@@ -6,7 +6,7 @@ import commons.functions as f
 object_specs = {
     "lov_season": {
         "dataset": d.dataset_specs["agg_team_performance_stats"]["dataset"],
-        "object_type" : "lov",
+        "object_type": "lov",
         "id": "lov_season",
         "label_col": "seasonId",
         "value_col": "seasonId",
@@ -16,13 +16,17 @@ object_specs = {
         "clearable": False,
         "placeholder": "Selecciona una Temporada",
         "multi": False,
-        "default_filters": None,
+        "default_filters": {
+            "aggregationType": "AGGREGATED",
+            "gameType2": "RS",
+            "groupingDescription": "MAJORLEAGUEID_SEASONID_GAMETYPE2_TEAMID",
+        },
         "callback_output": None,
         "callback_input": None,
     },
     "lov_majorLeague": {
         "dataset": d.dataset_specs["agg_team_performance_stats"]["dataset"],
-        "object_type" : "lov",
+        "object_type": "lov",
         "id": "lov_majorLeague",
         "label_col": "majorLeague",
         "value_col": "majorLeagueId",
@@ -32,13 +36,17 @@ object_specs = {
         "clearable": False,
         "placeholder": "Selecciona una Liga",
         "multi": False,
-        "default_filters": None,
+        "default_filters": {
+            "aggregationType": "AGGREGATED",
+            "gameType2": "RS",
+            "groupingDescription": "MAJORLEAGUEID_SEASONID_GAMETYPE2_TEAMID",
+        },
         "callback_output": None,
         "callback_input": None,
     },
     "lov_team": {
         "dataset": d.dataset_specs["agg_team_performance_stats"]["dataset"],
-        "object_type" : "lov",
+        "object_type": "lov",
         "id": "lov_team",
         "label_col": "teamName",
         "value_col": "teamId",
@@ -48,12 +56,39 @@ object_specs = {
         "clearable": False,
         "placeholder": "Selecciona un Equipo",
         "multi": True,
-        "default_filters": {"aggregationType": "AGGREGATED", "gameType2": "RS"},
-        "callback_output": [{"component_id": "lov_team", "component_property": "options"}],
-        "callback_input": [
-            {"component_id": "lov_majorLeague", "component_property": "value", "filter_col" : "majorLeagueId" },
-            {"component_id": "lov_season", "component_property": "value", "filter_col" : "seasonId" },
+        "default_filters": {
+            "aggregationType": "AGGREGATED",
+            "gameType2": "RS",
+            "groupingDescription": "MAJORLEAGUEID_SEASONID_GAMETYPE2_TEAMID",
+        },
+        "callback_output": [
+            {"component_id": "lov_team", "component_property": "options"}
         ],
+        "callback_input": [
+            {
+                "component_id": "lov_majorLeague",
+                "component_property": "value",
+                "filter_col": "majorLeagueId",
+            },
+            {
+                "component_id": "lov_season",
+                "component_property": "value",
+                "filter_col": "seasonId",
+            },
+        ],
+    },
+    "fig_winPercentage": {
+        "dataset": d.dataset_specs["agg_team_performance_stats"]["dataset"],
+        "object_type": "fig",
+        "id": "fig_winPercentage",
+        "default_filters": {
+            "aggregationType": "CUMULATIVE",
+            "gameType2": "RS",
+            "groupingDescription": "MAJORLEAGUEID_SEASONID_GAMETYPE2_TEAMID",
+        },
+        "fig": px.scatter(df, x="gameDate", y="winPercentage"),
+        "callback_output": None,
+        "callback_input": None
     },
 }
 
