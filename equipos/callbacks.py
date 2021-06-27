71e418c81dd6228d67d92ac1a543f9c5a069a791
Juanito Banca <jose_alvarado89@hotmail.com>
2021-06-27 16:19:56 -0700

    Update callbacks.py

--- equipos/callbacks.py
+++ equipos/callbacks.py
@@ -7,10 +7,26 @@ from app import app
 import commons.functions as f
 from equipos.specs import object_specs
 
-
+"""
 for fun in f.create_callback_functions_from_specs(object_specs=object_specs):
     exec(fun, locals())
+"""
 
+@app.callback(
+    Output(component_id="lov_team", component_property="options"),
+    [
+        Input(component_id="lov_majorLeague", component_property="value"),
+        Input(component_id="lov_season", component_property="value"),
+    ],
+)
+def lov_team(lov_majorLeague=None, lov_season=None):
+    filter_cols = {"majorLeagueId": lov_majorLeague, "seasonId": lov_season}
+    df = f.filter_df(df=object_specs["lov_team"]["dataset"], filter_cols=filter_cols)
+    obj = f.create_list_of_values(
+        df=df,
+        label_col=object_specs["lov_team"]["label_col"],
+        value_col=object_specs["lov_team"]["value_col"],
+    )
 
 
 '''
