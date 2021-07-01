5fae691e06a39b9bd35b9a6d5e15a5bb820db657
Juanito Banca <jose_alvarado89@hotmail.com>
2021-06-30 19:29:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash
 import dash_bootstrap_components as dbc
 
 # set app variable with dash, set external style to bootstrap theme
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY],
+app = dash.Dash(__name__,
         suppress_callback_exceptions = True,
         meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1'},],)
 
