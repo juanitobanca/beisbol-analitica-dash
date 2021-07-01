f1f98198f44403c59e778f4be616b3ed20f7263a
Juanito Banca <jose_alvarado89@hotmail.com>
2021-07-01 00:38:36 -0700

    Update layout.py

--- equipos/layout.py
+++ equipos/layout.py
@@ -37,12 +37,12 @@ for (lov, specs) in object_specs.items():
 
 control_container = html.Div(
     children = control_children,
-    style={'display': 'inline-block'}.
+    style={'display': 'inline-block'}
 )
 
 chart_container = html.Div(
-    children = chart_children,
-    style={'display': 'inline-block'}.
+        children = chart_children,
+    style={'display': 'inline-block'}
 )
 
 
