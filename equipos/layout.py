e722b6c2117ad65d14d11d10ba647a639a25ce94
Juanito Banca <jose_alvarado89@hotmail.com>
2021-07-05 15:44:06 -0700

    Update layout.py

--- equipos/layout.py
+++ equipos/layout.py
@@ -3,6 +3,8 @@
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table as dt
+
 
 # Custom dependencies
 from equipos.specs import object_specs
