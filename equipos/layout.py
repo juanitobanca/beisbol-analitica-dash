2e25fedd23434ba9257ceb04d776ae75e7de6b2b
Juanito Banca <jose_alvarado89@hotmail.com>
2021-07-05 16:39:27 -0700

    Update layout.py

--- equipos/layout.py
+++ equipos/layout.py
@@ -43,7 +43,7 @@ for (obj, specs) in object_specs.items():
         print("----------")
         object_specs[specs['container']]['children'].append(
             dbc.Col(
-                dt.DataTable( id = specs["id"], figure=specs["fig"] )
+                dt.DataTable( id = specs["id"], columns=specs["fig"]["columns"] )
             )
         )
 
