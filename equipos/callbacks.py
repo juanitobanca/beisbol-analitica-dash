1573f7406d09a715a300dbb399410bdf6783f1a8
Juanito Banca <jose_alvarado89@hotmail.com>
2021-07-05 19:25:51 -0700

    Update callbacks.py

--- equipos/callbacks.py
+++ equipos/callbacks.py
@@ -171,7 +171,9 @@ def fig_games(
     return obj
 
 @app.callback(
-    Output(component_id="table_games", component_property="data"),
+    [ Output(component_id="table_games", component_property="data"),
+      Output(component_id="table_games", component_property="table")
+     ],
     [
         Input(component_id="lov_majorLeague", component_property="value"),
         Input(component_id="lov_season", component_property="value"),
