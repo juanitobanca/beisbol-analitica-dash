59056e3f3fd80bfa628d4fb021276ed14796ac59
Juanito Banca <jose_alvarado89@hotmail.com>
2021-07-07 20:12:57 -0700

    Update specs.py

--- equipos/specs.py
+++ equipos/specs.py
@@ -369,8 +369,8 @@ object_specs = {
             "page_size" : 7,
             "columns" : {
             "Fecha" : { "id": "gameDate", "type" : "text", "presentation" : "text" },
-            "Boxscore" : { "id": "boxscoreUrl", "type" : "text", "presentation" : "markdown" },
-            "Jugada a Jugada" : { "id": "playByPlayUrl", "type" : "text", "presentation" : "markdown" },
+            #"Boxscore" : { "id": "boxscoreUrl", "type" : "text", "presentation" : "markdown" },
+            #"Jugada a Jugada" : { "id": "playByPlayUrl", "type" : "text", "presentation" : "markdown" },
             "Estadio" : { "id": "venueName", "type" : "text", "presentation" : "text" },
             "Equipo Local" : { "id": "homeTeamName", "type" : "text", "presentation" : "text" },
             "Carreras" : { "id": "resultadoCarreras", "type" : "text", "presentation" : "text" },
