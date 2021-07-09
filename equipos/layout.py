3669813ff2e21d653a6cb7cb4743d07e224f29c9
Juanito Banca <jose_alvarado89@hotmail.com>
2021-07-08 21:56:36 -0700

    Update layout.py

--- equipos/layout.py
+++ equipos/layout.py
@@ -34,7 +34,7 @@ for (obj, specs) in object_specs.items():
         object_specs[specs['container']]['children'].append(
             dbc.Col(html.Div(dcc.Graph(
                 id=specs["id"], figure=specs["fig"], config = specs['config']
-            )))
+            )), style = {'width': '33%'})
         )
 
     elif specs["object_type"] == "table":
