00e70c913565b333045535a24a4c7bab1beaf7c5
Juanito Banca <jose_alvarado89@hotmail.com>
2021-07-10 15:15:03 -0700

    Update functions.py

--- commons/functions.py
+++ commons/functions.py
@@ -203,8 +203,8 @@ def create_px_figure(df, fig_type, fig_specs):
     fig.update_layout(
             title={"y": 0.9, "x": 0.5, "xanchor": "center", "yanchor": "top"},
             #autosize=True,
-            width=400,
-            height=300,
+            #width=400,
+            #height=300,
             margin=dict(l=0, r=0, b=2, t=2, pad=0),
             showlegend=False,
             font=dict(size=10),
