1c19fceb67012b3a334efe36092cec51c79996cd
Juanito Banca <jose_alvarado89@hotmail.com>
2021-07-09 23:14:15 -0700

    Update specs.py

--- equipos/specs.py
+++ equipos/specs.py
@@ -438,8 +438,8 @@ object_specs = {
             "Dia/Noche" : { "id": "dayNight", "type" : "text", "presentation" : "text" },
             "Clima" : { "id": "weather", "type" : "text", "presentation" : "text" },
             },
-            "style_table" : { 'width': '100%' },
-            "fill_width" : False,
+            #"style_table" : { 'width': '100%' },
+            "fill_width" : True,
             "css" : [
                 {
                     'selector': 'table',
