a9413827cdbf15bfe21b2c7c1a5f6f6c571bc641
Juanito Banca <jose_alvarado89@hotmail.com>
2021-07-09 22:50:20 -0700

    Update navbar.py

--- navbar.py
+++ navbar.py
@@ -7,7 +7,7 @@ def Navbar():
             children=[
                 dbc.NavItem(dbc.NavLink("Jugadores", href="/jugadores")),
                 dbc.NavItem(dbc.NavLink("Estadios", href="/estadios")),
-                dbc.NavItem(dbc.NavLink("Equipos", href="/equipos2")),
+                dbc.NavItem(dbc.NavLink("Equipos", href="/equipos")),
                 dbc.NavItem(dbc.NavLink("Expectativa de Carrera", href="/expectativa")),
             ],
             brand="Béisbol Analítica",
