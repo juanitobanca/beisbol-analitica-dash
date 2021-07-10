b3c57245d4699f0d340c4a5fc6be8560044477a3
Juanito Banca <jose_alvarado89@hotmail.com>
2021-07-10 15:29:55 -0700

    Update layout.py

--- equipos/layout.py
+++ equipos/layout.py
@@ -91,13 +91,13 @@ layout = dbc.Container(
         dbc.Row(children=[html.Br()]),
         dbc.Row(
             children=[
-                dbc.Col(container_control, md = 2),
+                dbc.Col(container_control, width = 2),
                 dbc.Col(
                     children=[
                         dbc.Row(container_winPercentage),
                         html.Br(),
                         dbc.Row(container_games),
-                    ], md = 10
+                    ], width = 10
                 ),
             ],
         ),
