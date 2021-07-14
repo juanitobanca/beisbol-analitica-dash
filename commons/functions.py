05c00a522ea20724e08a848a3a8dd4d2af6c95b4
Juanito Banca <jose_alvarado89@hotmail.com>
2021-07-14 00:13:42 -0700

    Update functions.py

--- commons/functions.py
+++ commons/functions.py
@@ -192,7 +192,7 @@ def create_px_figure(df, fig_type, fig_specs):
 
     elif fig_type == "star":
         print("Returning a star")
-        df2 = df[['teamName']+fig_specs["metrics"]].iloc[0,:].reset_index()
+        df2 = df[['teamName']+fig_specs["metrics"]].iloc[0,:].reset_index(level='teamName')
         print(df2)
         fig = px.line_polar(
             df2,
