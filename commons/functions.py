d107ec483bf31c93881ca9dc02463d9e3b698233
Juanito Banca <jose_alvarado89@hotmail.com>
2021-08-15 12:53:42 -0700

    Update functions.py

--- commons/functions.py
+++ commons/functions.py
@@ -95,7 +95,7 @@ def filter_df(dataset_name, filter_cols, default_filters):
             print(f"Filtering by {column} : {value}")
             df = df[df[column] == value]
 
-    print(f"Length of dataset: {len(df)} for {dataset_name} and groupingDescription: {groupingDescription}")
+    print(f"Length of dataset: {len(df)} for {dataset_name} and default filters: {default_filters}")
     print("Returning dataset")
 
     return df
