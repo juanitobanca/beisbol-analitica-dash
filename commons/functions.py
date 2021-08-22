2d464e783cf48072514fbd0d6ea95496093ac074
Juanito Banca <jose_alvarado89@hotmail.com>
2021-08-22 13:52:16 -0700

    Update functions.py

--- commons/functions.py
+++ commons/functions.py
@@ -245,7 +245,7 @@ def create_px_figure(df, fig_type, fig_specs):
     elif fig_type == "heatmap4" :
 
         fig = go.Figure(go.Scatter(
-            x=[0, 1, 2, 2, 0], y=[-82, -81, -80, -253.45, -82], fill="toself"
+            x=[125, 126, 127, 127, 125], y=[43, 44, 45, 46.55, 43], fill="toself"
         ))
 
     fig.update_layout(
