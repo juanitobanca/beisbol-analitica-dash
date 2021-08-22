e32d22d206c789256381c69a02c071a8816c16c3
Juanito Banca <jose_alvarado89@hotmail.com>
2021-08-22 14:05:50 -0700

    Update functions.py

--- commons/functions.py
+++ commons/functions.py
@@ -256,8 +256,8 @@ def create_px_figure(df, fig_type, fig_specs):
                     fill="toself",
                 ),
                 go.Scatter(
-                    x=[125, 126, 127, 127, 125],
-                    y=[43, 44, 45, 46.55, 43],
+                    x=[125, 126, 127,  0,  125, ],
+                    y=[43, 45.41, 47.82, 125, 43 ],
                     fill="toself",
                 ),
             ]
