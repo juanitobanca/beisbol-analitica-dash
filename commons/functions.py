9dbaec241d329b60ac5dfcf225cf46709a575b73
Juanito Banca <jose_alvarado89@hotmail.com>
2021-09-12 17:12:51 -0700

    Update functions.py

--- commons/functions.py
+++ commons/functions.py
@@ -346,7 +346,7 @@ def create_px_figure(df, fig_type, fig_specs):
                     fill="toself",
                     mode="markers",
                     marker=dict(
-                        color='goldenrod',
+                        color='lightyellow',
                         size=0.5,
                     ),
                 ),
@@ -398,7 +398,7 @@ def create_px_figure(df, fig_type, fig_specs):
                     fill="toself",
                     mode="markers",
                     marker=dict(
-                        color='indianred',
+                        color='orangered',
                         size=0.5,
                     ),
                 ),
@@ -450,7 +450,7 @@ def create_px_figure(df, fig_type, fig_specs):
                     fill="toself",
                     mode="markers",
                     marker=dict(
-                        color='goldenrod',
+                        color='lightyellow',
                         size=0.5,
                     ),
                 ),
