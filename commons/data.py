30bbc4a3fdadfdcb8dc207d22067eda7077f13b7
Juanito Banca <jose_alvarado89@hotmail.com>
2021-09-24 22:22:32 -0700

    pitches

--- commons/data.py
+++ commons/data.py
@@ -74,122 +74,146 @@ dataset_specs = {
         ),
         "column_renamings": None,
     },
+    "pitches": {
+        "path": "datasets/pitches.csv",
+        "format": "csv",
+        "column_renamings": None,
+        "query": """
+        With p AS
+        (
+        Select gamePk, battingTeamId, batterId, pitchingTeamId, pitcherId, coordX, coordY, trajectory, HM4, HM8
+        From pitches
+        Where coordX Is Not Null
+        And coordY Is Not Null
+        ), g As
+        (
+        Select gamePk, seasonId, majorLeagueId
+        From games
+        Where gameType2 = 'RS'
+        And seasonId > 2012
+        )
+        Select g.seasonId, g.majorLeagueId, p.*
+        From g
+        Inner Join p
+        On g.gamePk = p.gamePk
+        """
+    },
     "games": {
         "path": "datasets/games.csv",
         "format": "csv",
         "column_renamings": None,
         "query": """
-WITH g2 AS (
-  SELECT
-    homeTeamId AS teamId,
-    'home' AS teamType,
-    gamePk,
-    gameType2,
-    majorLeagueId,
-    seasonId,
-    gameDate,
-    venueName,
-    homeTeamName,
-    awayTeamName,
-    CONCAT(homeScore, '-', awayScore) resultadoCarreras,
-    IF(homeIsWinner > 0, 'Ganado', 'Perdido') resultado,
-    attendance,
-    doubleHeader,
-    dayNight,
-    weather,
-    wind,
-    homeScore - awayScore runDifference
-  FROM games
+        WITH g2 AS (
+        SELECT
+            homeTeamId AS teamId,
+            'home' AS teamType,
+            gamePk,
+            gameType2,
+            majorLeagueId,
+            seasonId,
+            gameDate,
+            venueName,
+            homeTeamName,
+            awayTeamName,
+            CONCAT(homeScore, '-', awayScore) resultadoCarreras,
+            IF(homeIsWinner > 0, 'Ganado', 'Perdido') resultado,
+            attendance,
+            doubleHeader,
+            dayNight,
+            weather,
+            wind,
+            homeScore - awayScore runDifference
+        FROM games
 
-  UNION ALL
+        UNION ALL
 
-  SELECT
-    awayTeamId AS teamId,
-    'away' AS teamType,
-    gamePk,
-    gameType2,
-    majorLeagueId,
-    seasonId,
-    gameDate,
-    venueName,
-    homeTeamName,
-    awayTeamName,
-    CONCAT(homeScore, '-', awayScore) resultadoCarreras,
-    IF(awayIswinner > 0, 'Ganado', 'Perdido') resultado,
-    attendance,
-    doubleHeader,
-    dayNight,
-    weather,
-    wind,
-    awayScore - homeScore runDifference
-  FROM games
-), g As
-(
-SELECT
-    gameType2,
-    majorLeagueId,
-    seasonId,
-    teamId,
-    teamType,
-    gameDate,
-    venueName,
-    homeTeamName,
-    awayTeamName,
-    resultadoCarreras,
-    resultado,
-    attendance,
-    runDifference,
-                REPLACE(REPLACE(REPLACE(REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE (
-        REPLACE( REPLACE( REPLACE( REPLACE(weather,'0','')
-        ,'1',''),'2',''),'3',''),'4',''),'5',''),'6',''),'7',''),'8',''),'9',''), 'degrees', '' ), '.',''), ' , ', '') weather,
-        wind,     CONCAT('[Boxscore](https://www.milb.com/gameday/', gamePk, '#game_state=final)') boxscoreUrl,
-    CONCAT(
-      '[Jugada a Jugada](https://www.milb.com/gameday/',
-      gamePk,
-      '#game_tab=play-by-play)'
-    ) playByPlayUrl,
-        CASE
-      WHEN doubleHeader IN ('N', 'S') THEN ''
-      ELSE 'Doble Juego'
-    END doubleHeader,
-    CASE
-      WHEN dayNight = 'night' THEN 'Dia'
-      ELSE 'Noche'
-    END dayNight
-FROM g2
-)
-Select  gameType2,
-    majorLeagueId,
-    seasonId,
-    teamId,
-    teamType,
-    gameDate,
-    venueName,
-    homeTeamName,
-    awayTeamName,
-    resultadoCarreras,
-    resultado,
-    attendance,
-    playByplayUrl,
-    boxScoreUrl,
-    doubleHeader,
-    dayNight,
-    runDifference,
-    Case When weather = 'Partly Cloudy' Then 'Parcialmente Nublado'
-            When weather In ( 'Overcast', 'Cloudy' ) Then 'Nublado'
-            When weather = 'Snow' Then 'Nevado'
-            When weather = 'Sunny' Then 'Soleado'
-            When weather in ('Drizzle', 'Rain' ) Then 'Lluviado'
-            When weather = 'Roof Closed' Then 'Techo Cerrado'
-            When weather = ('Clear' ) Then 'Despejado'
-            When weather Is Null Or weather = 'Unknown' Then 'Desconocido'
-            Else weather
-       End weather,
-       wind
-From g
-Where seasonId >= 2010
-ORDER BY
-  gameDate
+        SELECT
+            awayTeamId AS teamId,
+            'away' AS teamType,
+            gamePk,
+            gameType2,
+            majorLeagueId,
+            seasonId,
+            gameDate,
+            venueName,
+            homeTeamName,
+            awayTeamName,
+            CONCAT(homeScore, '-', awayScore) resultadoCarreras,
+            IF(awayIswinner > 0, 'Ganado', 'Perdido') resultado,
+            attendance,
+            doubleHeader,
+            dayNight,
+            weather,
+            wind,
+            awayScore - homeScore runDifference
+        FROM games
+        ), g As
+        (
+        SELECT
+            gameType2,
+            majorLeagueId,
+            seasonId,
+            teamId,
+            teamType,
+            gameDate,
+            venueName,
+            homeTeamName,
+            awayTeamName,
+            resultadoCarreras,
+            resultado,
+            attendance,
+            runDifference,
+                        REPLACE(REPLACE(REPLACE(REPLACE( REPLACE( REPLACE( REPLACE( REPLACE( REPLACE (
+                REPLACE( REPLACE( REPLACE( REPLACE(weather,'0','')
+                ,'1',''),'2',''),'3',''),'4',''),'5',''),'6',''),'7',''),'8',''),'9',''), 'degrees', '' ), '.',''), ' , ', '') weather,
+                wind,     CONCAT('[Boxscore](https://www.milb.com/gameday/', gamePk, '#game_state=final)') boxscoreUrl,
+            CONCAT(
+            '[Jugada a Jugada](https://www.milb.com/gameday/',
+            gamePk,
+            '#game_tab=play-by-play)'
+            ) playByPlayUrl,
+                CASE
+            WHEN doubleHeader IN ('N', 'S') THEN ''
+            ELSE 'Doble Juego'
+            END doubleHeader,
+            CASE
+            WHEN dayNight = 'night' THEN 'Dia'
+            ELSE 'Noche'
+            END dayNight
+        FROM g2
+        )
+        Select  gameType2,
+            majorLeagueId,
+            seasonId,
+            teamId,
+            teamType,
+            gameDate,
+            venueName,
+            homeTeamName,
+            awayTeamName,
+            resultadoCarreras,
+            resultado,
+            attendance,
+            playByplayUrl,
+            boxScoreUrl,
+            doubleHeader,
+            dayNight,
+            runDifference,
+            Case When weather = 'Partly Cloudy' Then 'Parcialmente Nublado'
+                    When weather In ( 'Overcast', 'Cloudy' ) Then 'Nublado'
+                    When weather = 'Snow' Then 'Nevado'
+                    When weather = 'Sunny' Then 'Soleado'
+                    When weather in ('Drizzle', 'Rain' ) Then 'Lluviado'
+                    When weather = 'Roof Closed' Then 'Techo Cerrado'
+                    When weather = ('Clear' ) Then 'Despejado'
+                    When weather Is Null Or weather = 'Unknown' Then 'Desconocido'
+                    Else weather
+            End weather,
+            wind
+        From g
+        Where seasonId >= 2010
+        ORDER BY
+        gameDate
         """,
     },
 }
