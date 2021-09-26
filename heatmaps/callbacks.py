ed3b606cfab7509bedbb0ca68398dc21c65e925e
Juanito Banca <jose_alvarado89@hotmail.com>
2021-09-25 17:59:30 -0700

    Update callbacks.py

--- heatmaps/callbacks.py
+++ heatmaps/callbacks.py
@@ -1,4 +1,3 @@
-
 # Dash components, html, and dash tables
 from dash.dependencies import Input, Output
 
@@ -6,3 +5,37 @@ from dash.dependencies import Input, Output
 from app import app
 import commons.functions as f
 from heatmaps.specs import object_specs
+
+
+@app.callback(
+    Output(component_id="fig_contour", component_property="figure"),
+    [
+        Input(component_id="lov_majorLeague_hm", component_property="value"),
+        Input(component_id="lov_season_hm", component_property="value"),
+        Input(component_id="lov_player_hm", component_property="value"),
+        Input(component_id="lov_trajectory_hm", component_property="value"),
+    ],
+)
+def fig_contour(
+    lov_majorLeague_hm, lov_season_hm=None, lov_player_hm=None, lov_trajectory_hm=None
+):
+    print(f"Calling from fig_contour")
+    filter_cols = {
+        "majorLeagueId": lov_majorLeague_hm,
+        "seasonId": lov_season_hm,
+        "batterId": lov_player_hm,
+        "trajectory": lov_trajectoy_hm,
+    }
+    df = f.filter_df(
+        dataset_name=object_specs["fig_contour"]["dataset_name"],
+        filter_cols=filter_cols,
+        default_filters=object_specs["fig_contour"]["default_filters"],
+    )
+    obj = f.create_px_figure(
+        df=df,
+        fig_type=object_specs["fig_contour"]["fig_type"],
+        fig_specs=object_specs["fig_contour"]["fig_specs"],
+    )
+
+    print(obj)
+    return obj
