184ff585b915a8249c2681fb40c1efcd7d489a22
Juanito Banca <jose_alvarado89@hotmail.com>
2021-09-26 18:29:56 -0700

    Update functions.py

--- commons/functions.py
+++ commons/functions.py
@@ -78,6 +78,9 @@ def filter_df(dataset_name, filter_cols, default_filters):
 
         if d.dataset_specs[dataset_name]["column_renamings"]:
             df = df.rename(columns=d.dataset_specs[dataset_name]["column_renamings"])
+    else:
+
+        df = d.dataset_specs[dataset_name]["dataset"]
 
     filters = {}
 
