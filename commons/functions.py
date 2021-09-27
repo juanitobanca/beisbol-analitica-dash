b28c3a5c6d81072d16ad50bafbdca39f200e62b3
Juanito Banca <jose_alvarado89@hotmail.com>
2021-09-26 19:39:04 -0700

    Update functions.py

--- commons/functions.py
+++ commons/functions.py
@@ -266,9 +266,7 @@ def create_px_figure(df, fig_type, fig_specs):
             go.Histogram2dContour(
                 x=df["coordX"],
                 y=df["coordY"],
-                colorscale="Hot",
-                xaxis = 'X',
-                yaxis = 'Y'
+                colorscale="Hot"
             )
         )
         fig.update_traces(contours_coloring="fill", contours_showlabels=True)
